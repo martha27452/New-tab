3da9959fb9cfeed1adeb609e287f2b9462b92bfe
martha27452 <47460668+martha27452@users.noreply.github.com>
2025-03-03 06:50:50 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const loader = new THREE.TextureLoader();
+loader.load('https://drive.google.com/uc?export=view&id=1dVss5IOZiKvW-RRQ87X5l7H0uq-tRQmo', 
+    function (texture) {
+		const material = new THREE.SpriteMaterial({ map: texture });
+		const sprite = new THREE.Sprite(material);
+		scene.add(sprite);
+		function animate() {
+			requestAnimationFrame(animate);
+			sprite.rotation.z += 0.01;
+			renderer.render(scene, camera);
+		}
+		animate();
+	},
+	null,
+	function (error) {
+		console.error('An error occurred loading the texture:', error);
+	}
+);
