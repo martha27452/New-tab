bef9fd7a44418f84fdf67af1bf12f563e59d6234
martha27452 <47460668+martha27452@users.noreply.github.com>
2025-03-03 07:43:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 1;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -22,3 +22,10 @@ loader.load('assets/shogun_cac.png',
 		animate();
 	}
 );
+window.addEventListener('resize', () => {
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	const aspect = window.innerWidth / window.innerHeight;
+	sprite.scale.set(aspect, 1, 1);
+});
